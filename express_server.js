dbd672d15a12c6cfca4fbaa3cb0a7e17054f19d4
Manpreet <mp_manpreet@hotmail.com>
2023-03-23 04:44:14 +0000

    slight changes - added /urls.json

--- express_server.js
+++ express_server.js
@@ -11,6 +11,11 @@ app.get('/', (req, res) => {
   res.send("Hello to my first server!");
 });
 
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}.`);
 });
+
