7365539fe5b2564f4cde3e8b4f9f7ad1d4f7d0db
Manpreet <mp_manpreet@hotmail.com>
2023-03-25 07:12:59 +0000

    added generate string function, urls_new, and attempted to change css but didnt work

--- express_server.js
+++ express_server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const app = express();
 const PORT = 8080; // this will be our default
-
 app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -26,9 +26,18 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 }); // linked to file in views urls_index
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+})
+
 app.get("/urls/:id", (req, res) => {
   const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
   res.render("urls_show", templateVars);
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
 });
 
 app.listen(PORT, () => {
@@ -39,4 +48,17 @@ app.listen(PORT, () => {
 
 // app.get("/urls-show", (req, res) => {
 //   res.render("urls_show", templateVars)
-// })
+// }) - this isnt the proper syntax
+
+function generateRandomString(input) {
+  let characters = "abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  let stringResult = '';
+
+  for (let i = 0; i < input.length; i++) {
+   let random = Math.floor(Math.random() * characters.length);
+    stringResult += characters[random];
+  }
+  return stringResult;
+}
+
+// console.log(generateRandomString('asdadadasda'));
