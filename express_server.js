5a433a96aa32beb49610bb894cd46bf0049e6e11
Manpreet <mp_manpreet@hotmail.com>
2023-03-25 06:28:30 +0000

    added partial header in urls

--- express_server.js
+++ express_server.js
@@ -17,15 +17,26 @@ app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
+app.get("/hello", (req, res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
+
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
-});
+}); // linked to file in views urls_index
 
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
+app.get("/urls/:id", (req, res) => {
+  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
+  res.render("urls_show", templateVars);
 });
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}.`);
 });
+
+//--------------------------------------
+
+// app.get("/urls-show", (req, res) => {
+//   res.render("urls_show", templateVars)
+// })
